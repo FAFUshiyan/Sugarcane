a213491b6ec733aa5dabf20bfae2fc6aecf75fc9
FAFUshiyan <76734644+FAFUshiyan@users.noreply.github.com>
2020-12-30 17:18:58 +0800

    Update get_allele_variation.pl

--- get_allele_variation.pl
+++ get_allele_variation.pl
@@ -4,44 +4,59 @@ $/='>';
 <IN>;
 while(<IN>){
 chomp;
+my $nac = 0;
+my $nca = 0;
+my $nag = 0;
+my $nga = 0;
+my $ntc = 0;
+my $nct = 0;
+my $ntg = 0;
+my $ngt = 0;
+my $ncg = 0;
+my $ngc = 0;
+my $nat = 0;
+my $nta = 0;
 my ($name,$seq) = split(/\n/,$_,2);
 my @tmp = split (/\n/,$seq);
 my @seqa = split (//,$tmp[0]);
 my @seqb = split (//,$tmp[1]);
- foreach $i(0..$#seqa){
-        my      $sa = $seqa[$i];
-                print "$sa";
-                }
-        print "\n";
- foreach $i(0..$#seqb){
-        my $sb = $seqb[$i];
-        print "$sb";
-        }
-print "\n";
+print "$name\t";
+$len = @seqb;
         foreach $i(0..$#seqa){
         my      $sae = $seqa[$i];
         my      $sbe = $seqb[$i];
         my  $two = $seqa[$i].$seqb[$i];
-                if ($sae eq  "-" or $sbe eq "-"){
-                        print "#";
+                if ($sae eq  '-' or $sbe eq '-'){
                         }elsif($sae eq $sbe){
-                        print "*";
-                        }elsif(($two eq 'at') or ($two eq 'ta')){
-                        print "M";
-                        }elsif(($two eq 'cg') or ($two eq 'gc')){
-             print "N";
-             }elsif(($two eq 'ac') or ($two eq 'ca')){
-             print "J";
-             }elsif(($two eq 'ag') or ($two eq 'ga')){
-             print "K";
-             }elsif(($two eq 'tc') or ($two eq 'ct')){
-             print "Q";
-             }elsif(($two eq 'tg') or ($two eq 'gt')){
-             print "W";
-             }else{
-                        print " ";
+                        }elsif($two eq 'at'){
+                                                        $nat++;
+                        }elsif($two eq 'ta'){
+                                                        $nta++;
+                        }elsif($two eq 'cg'){
+                                                        $ncg++;
+             }elsif ($two eq 'gc'){
+                                                        $ngc++;
+                }elsif($two eq 'ac'){
+                                                        $nac++;
+             }elsif($two eq 'ca'){
+                                                        $nca++;
+                        }elsif($two eq 'ag'){
+                                                        $nag++;
+             }elsif($two eq 'ga'){
+                                                        $nga++;
+                        }elsif($two eq 'tc'){
+                                                        $ntc++;
+             }elsif($two eq 'ct'){
+                                                        $nct++;
+                        }elsif($two eq 'tg'){
+                                                        $ntg++;
+             }elsif($two eq 'gt'){
+                                                        $ngt++;
+                        }else{
                         }
+
         }
-        print "\n";
+        $to = $nac+$nag+$ntc+$ntg+$nat+$ncg+$nca+$nga+$nct+$ngt+$nta+$ngc;
+        print "A=>C\t$nac\tC=>A\t$nca\tA=>G\t$nag\tG=>A\t$nga\tT=>C\t$ntc\tC=>T\t$nct\tT=>G\t$ntg\tG=>T\t$ngt\tA=>T\t$nat\tT=>A\t$nta\tC=>G\t$ncg\tG=>C\t$ngc\ttotal\t$to\tseq_len\t$len\n";
 }
 close IN;
